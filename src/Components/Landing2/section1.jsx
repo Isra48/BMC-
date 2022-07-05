3437730503b3307a402d693dbf63439468845b0f
Israwortev <81185836+Israwortev@users.noreply.github.com>
2022-07-05 01:01:26 -0500

    Media queries BMC almost finished

--- src/Components/Landing2/section1.jsx
+++ src/Components/Landing2/section1.jsx
@@ -3,23 +3,25 @@ import logo from "../../images/mac2.png";
 
 const Section1 = (props) => {
   return (
-    <div className="seccion1-container">
-      <div className="Section1">
-        <div className="child1">
-          <img
-            className="mockmac"
-            src={logo}
-            alt="Logo de Wortev Aceleradora"
-          />
-        </div>
-        <div className="child2">
-          <h1 className="TituloPrincipal">BMC</h1>
-          <p className="plight">Business Model Canvas</p>
-          <p className="pnormal">Estructura y reinventa tu empresa</p>
-          <div className="btnContainer">
-            <button className="btnModal" onClick={props.click}>
-              Ingresar
-            </button>
+    <div className="mastersec1">
+      <div className="seccion1-container">
+        <div className="Section1">
+          <div className="child1">
+            <img
+              className="mockmac"
+              src={logo}
+              alt="Logo de Wortev Aceleradora"
+            />
+          </div>
+          <div className="child2">
+            <h1 className="TituloPrincipal">BMC</h1>
+            <p className="plight">Business Model Canvas</p>
+            <p className="pnormal">Estructura y reinventa tu empresa</p>
+            <div className="btnContainer">
+              <button className="btnModal" onClick={props.click}>
+                Ingresar
+              </button>
+            </div>
           </div>
         </div>
       </div>
